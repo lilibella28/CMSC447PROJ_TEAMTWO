5adf1ce6ae6fd4af0c6373cc58c3cef330dc4e84
lilibella28 <lilibella28@hotmail.com>
2025-10-15 13:34:16 -0400

    admin models

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,14 @@
 from . import db
 
+
+
+
+class Admin(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
+    # add password check and hash
+
+
 class Test(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100))
